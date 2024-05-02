188aff31d0675a2824d41ceaf7504209add50f78
mateusleitao <mateus@mateus>
2024-05-02 13:19:37 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express')
+
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+const app = express()
+
+app.get('/', (req, res) => {
+    // const filePath = path.join(__dirname, 'index.js');
+    
+    res.send("Hello World");
+});
+    
+app.listen(PORT, HOST);
